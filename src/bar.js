afafcb5ae3bda754e739e2441aafd4ca47e27bf0
josephheinz <josephheinz28@gmail.com>
2024-09-16 14:31:41 -0400

    moved progressbar to bar js
    made more sense there

--- src/bar.js
+++ src/bar.js
@@ -27,4 +27,60 @@ class ProgressBar {
   }
 }
 
+function progressBar(time, bar) {
+  if (bar.active) return;
+
+  bar.active = true;
+
+  let interval = 50;
+  let oppInterval = 1000 / interval;
+  let runs = 0;
+
+  bar.setMax(time);
+  bar.setValue(0);
+  bar.setWidth();
+
+  let dtime = Date.now();
+
+  let intervalLoop = setInterval(() => {
+    // move the bar 1/20 of 1s worth of width
+    // not sure why i have to divide by time again
+    // maybe ill figure it out eventually
+    bar.setValue(((time / oppInterval ) * runs) / time);
+    bar.setWidth();
+
+    runs++;
+    //console.log(runs, (time / 20) * (runs - 1), bar.value);
+
+    if (runs >= time * ( 1000 / interval ) ) {
+      clearInterval(intervalLoop);
+      bar.setValue(bar.max);
+      bar.setWidth();
+      let now = Date.now();
+
+      let elapsed = (now - dtime) / 1000;
+
+
+      /* 
+         this console log is doing a lot
+         all of the math is just finding the percentage difference between the 
+         inputted time and the actual elapsed time
+
+         | time - elapsed |
+         ------------------   x   100  =   Percent Difference
+         [ time + elapsed ]
+         [----------------]
+         [       2        ]
+        it gets converted to a substring so its not 20 characters long
+      */
+      console.log(
+        `Time elapsed: ${elapsed}\n${
+          ( abs((time - elapsed) / ((time + elapsed) / 2)) * 100 ).toString().substring(0,6)
+        }% Variance`
+      );
+      bar.active = false;
+    }
+  }, interval);
+}
+
 let mineBar = new ProgressBar(100, 100, document.querySelector("#test"));
