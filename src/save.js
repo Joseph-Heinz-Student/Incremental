381368e6fbc190a5733499c16745b79024b25c13
Joseph Heinz <joheb41@student.oaisd.org>
2024-09-17 10:43:32 -0400

    savepoint

--- src/save.js
+++ src/save.js
@@ -1,10 +1,24 @@
 const ls = localStorage;
 
-function save (_game) {
-    ls.gameSave = JSON.stringify(_game);
-    return ls.gameSave;
+function save(_game) {
+  ls.gameSave = JSON.stringify(_game);
+  return ls.gameSave;
 }
 
-function load (_game) {
-    return
+function load(_game) {
+  if (ls.gameSave != null && ls.gameSave != "undefined") {
+    let gameClone = JSON.parse(ls.gameSave);
+    for (resource in gameClone.resources) {
+        if (gameClone.resources[resource] == null) {
+            gameClone.resources[resource] = _game.resources[resource];
+        }
+    }
+
+    _game = gameClone;
+
+    
+
+  }
+
+  return _game;
 }
